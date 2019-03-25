edaca9858e475509ceef1dd345a40483b5f475f6
lmatthews991 <45792805+lmatthews991@users.noreply.github.com>
2019-03-25 09:49:55 +0000

    Update Bike_Sensor.py

--- Bike_Sensor.py
+++ Bike_Sensor.py
@@ -1,36 +1,49 @@
 import RPi.GPIO as GPIO
 import time
 import csv
+import datetime
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-start = time.time()
-wheel_c = 0
-data =[]
+t = time.time()
+t2 = time.time()
 distance = 0
-rpm = 0.00
-speed = 0.00
-elapsed = 0.00
+wheel_c = 20
 
+with open('testcsv.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(["timestamp", "elapsed", "rpm", "speed", "distance", "duration"])
+f.close()
 
 while True:
     input_state = GPIO.input(18)
     if input_state == False:
         timestamp = time.time()
         stamp = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M:%S')
-        elapsed = time.time()
+        newt = time.time()
+        elapsed = newt - t
+        duration = time.time() - t2
         distance += wheel_c
         rpm = 1/elapsed *60
         speed = (wheel_c*rpm*60)/1000
-        print('Button Pressed', elapsed - start, distance, rpm, speed, timestamp, stamp)
-        Start = elapsed
+        print('Sensor Triggered', elapsed, distance, duration, rpm, speed, timestamp, stamp)
+        t = newt
         time.sleep(0.2)
-        data = [[rpm,speed,distance,elapsed,timestamp, stamp]]
+        
+        
+        # Store Data in List
+        data = [[stamp, elapsed, rpm, speed, distance, duration]]
         print(data)
+        
+        #Append to csv 
         with open('testcsv.csv', 'a') as f:
             writer = csv.writer(f)
             for row in data:
                 writer.writerows(data)
         f.close()
+        
+        # Reinitialise Start
+        
+        time.sleep(0.2)   
+       
